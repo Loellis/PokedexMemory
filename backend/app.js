0d751453782922bd4d33c44a6dc7ac0610ac7d28
Martin Kobro <loellis92@gmail.com>
2024-02-13 14:01:13 +0100

    Added cors to backend.

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,9 @@ const config = require("./utils/config")
 const express = require('express')
 const app = express()
 const pokemonRoutes = require("./routes/pokedexMemRoute");
-const logger = require("./utils/logger")
+const cors = require("cors")
 
+app.use(cors())
 app.use('/api', pokemonRoutes);
 
 module.exports = app
